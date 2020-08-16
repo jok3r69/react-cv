4352c546924cd0602b71964d894e751e0fccc278
Kondrat Kristof <kristof.kondrat@gmail.com>
2020-08-16 08:14:30 +0300

    created portfolio website

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
 import '../App.css'
+import * as fontawesome from '@fortawesome/fontawesome-svg-core';
+import {
+    FaGit,
+    FaGithub
+} from 'react-icons/fa';
 
 function Portfolio() {
 
     return (
         <>
+        <div className="portfolio-cards">
+        <div className="container">
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <div className="icon">
+                       <FaGithub className="githubIcon" aria-hidden="true"/>
+                    </div>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <h3>
+                        <a href="https://github.com/jok3r69/xoxoxoxo" target="_blank">Tic Tac Toe</a>
+                    </h3>
+                    <p>Wanna play a game? This is the vintage tic tac toe</p>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <div className="icon">
+                    <FaGithub className="githubIcon" aria-hidden="true"/>
+                    </div>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <h3>
+                        <a href="https://github.com/jok3r69/CSS-webshop" target="_blank">Webshop template link</a>
+                    </h3>
+                    <p>I also made a template for a webshop.</p>
+                </div>
+            </div>
+        </div>
+        <div className="card">
+            <div className="face face1">
+                <div className="content">
+                    <div className="icon">
+                    <FaGithub className="githubIcon" aria-hidden="true"/>
+                    </div>
+                </div>
+            </div>
+            <div className="face face2">
+                <div className="content">
+                    <h3>
+                        <a href="https://github.com/jok3r69/react-cv" target="_blank">Awesome website link</a>
+                    </h3>
+                    <p>Check out the code I wrote for this website</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
         </>
     );
 }
