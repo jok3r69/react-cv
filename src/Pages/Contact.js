b06b2e495e912ba2dfa1917fc38f9fe25b2b31b8
jok3r69 <kristof.kondrat@gmail.com>
2020-08-19 19:56:16 +0300

    Update Contact.js

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -7,6 +7,7 @@ import '../App.css';
 function ContactPage() {
   const [showModal, setShowModal] = useState(false);
   function sendEmail(e) {
+    console.log(e.target);
     e.preventDefault();
 
     emailjs.sendForm('gmail', 'template_test', e.target, 'user_qzbTJOI9o6aAwYWhA9ywd')
@@ -24,16 +25,16 @@ function ContactPage() {
       <div className="container">
         <h1 className="contact-h1">For more info, feel free to contact me!</h1>
         <div className="form-container">
-          <form action="action_page.php" onSubmit={sendEmail}>
+          <form  onSubmit={sendEmail}>
 
             <label form="fname">Full Name</label>
-            <input type="text" id="fname" name="fullname" data-displayname="fullname" placeholder="Your full name.."></input>
+            <input type="text" name="fullname" data-displayname="fullname" placeholder="Your full name.."></input>
 
             <label form="email">Your eMail</label>
-            <input type="text" name="email" id="email" data-displayname="email"placeholder="test@test.com"></input>
+            <input type="text" name="email" data-displayname="email"placeholder="test@test.com"></input>
 
             <label form="subject">Subject</label>
-            <textarea id="subject" name="subject" data-displayname="subject" placeholder="Feel free to contact me..."></textarea>
+            <textarea name="subject" data-displayname="subject" placeholder="Feel free to contact me..."></textarea>
 
             <input type="submit" value="Send"></input>
 
